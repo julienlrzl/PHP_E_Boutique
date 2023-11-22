9c24b0d136b9b855d6953c0e537e5a4b22839e3a
julien larzul <julien.larzul@etu.univ-lyon1.fr>
2023-11-22 18:22:22 +0100

    ajout bootstrap

--- ECommerce/twig-config.php
+++ ECommerce/twig-config.php
@@ -2,6 +2,6 @@
 
 require_once 'vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader("View");
+$loader = new \Twig\Loader\FilesystemLoader("app/View/templates");
 $twig = new \Twig\Environment($loader);
 echo $twig->render('index.twig');
